0d5d7f8cbc600b2546b0a0b81b2de72bdbc07310
kripal <thakurkripalsingh6@gmail.com>
2026-01-08 20:50:20 +0530

    login

--- index.js
+++ index.js
@@ -76,7 +76,7 @@ app.get("/",(req, res)=>{
 });
 app.use("/listings",listingRouter);
 app.use("/listings/:id/reviews",reviewRouter);
-app.use("/", userRouter)
+app.use("/", userRouter);
 
 
 app.all(/.*/,(req,res,next)=>{
