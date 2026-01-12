8334a7a73b71bb90d14191df6a94113d6b4c5c4d
kripal <thakurkripalsingh6@gmail.com>
2026-01-12 22:37:07 +0530

    redirect path

--- middleware.js
+++ middleware.js
@@ -1,4 +1,5 @@
 module.exports.isLoggedIn= (req,res,next) => {
+   console.log(req.path,"..", req.originalUrl);
      if(!req.isAuthenticated()){
         req.flash("error","You must be logged in to creating listing");
        return res.redirect("/login")
